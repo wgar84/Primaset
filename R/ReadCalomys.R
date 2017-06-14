a031e4c09a5c8cc5645579871480be1f8cef5ecf
Guilherme Garcia <wgar@usp.br>
2017-06-14 16:36:59 -0300

    started ReadCalomys

---
+++ R/ReadCalomys.R
@@ -0,0 +1,25 @@
+#' @title
+#' ReadCalomys
+#'
+#' @description
+#' This function reads Excel files containing the Calomys colony data.
+#'
+#' @param
+#' file
+#'
+#' @return
+#' list with three (?) elements
+#'
+#' @author Guilherme Garcia
+#'
+#' @importFrom gdata read.xls
+#' @importFrom plyr aaply
+#'
+#' @examples
+#'
+#' 
+#'
+ReadCalomys <- function(file)
+{
+    
+}
