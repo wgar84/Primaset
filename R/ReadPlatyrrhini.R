0801ae2cb0710967b121d67c2fb59951d7690865
Guilherme Garcia <wgar@usp.br>
2017-06-17 22:27:43 -0300

    still working on ReadPlatyrrhini

--- R/ReadPlatyrrhini.R
+++ R/ReadPlatyrrhini.R
@@ -12,7 +12,7 @@
 ##' @author Guilherme Garcia
 ##'
 ##' @importFrom gdata read.xls
-##' @importFrom plyr aaply
+##' @importFrom plyr aaply alply
 ##'
 ##' @examples
 ##'
@@ -20,14 +20,49 @@
 
 ReadPlatyrrhini <- function(path)
 {
-    id <- read.csv(paste0(path, '/semsp12.csv'))
+    id.raw <- read.csv(paste0(path, '/semsp12.csv'))
 
+    id.clean <- read.csv(paste0(path, '/GLMALL2_nodist.csv'))
+    
     ## repeated individuals
-    id <- id[- which(duplicated(id [, 1])), ]
+    id.raw <- id.raw[- which(duplicated(id.raw $ ID.)), ]
     ## só 4
 
-    num <- platyrrhini.id [, 1]
+    id.clean <- id.clean[- which(duplicated(id.clean $ ID)), ]
+    ## só 2 (que tb se repetiam no raw)
 
+    num.raw <- as.character(id.raw $ ID.)
+    num.clean <- as.character(id.clean $ ID)
 
+    id.raw <- id.raw [num.raw %in% num.clean, ]
+    num.raw <- num.raw [num.raw %in% num.clean]
+    
+    A <- dir(path, pattern = '\\.A', recursive = TRUE, ignore.case = TRUE)
+    Z <- dir(path, pattern = '\\.Z', recursive = TRUE, ignore.case = TRUE)
+
+    ### para cada elemento, achar os arquivos relevantes
+    
+    file.list <-
+        alply(num.raw, 1,
+              function(N)
+              {
+                  N.A <- paste0(N, '.A')
+                  N.Z <- paste0(N, '.Z')
+                  files <- c(dir(path, pattern = N.A, recursive = TRUE),
+                             dir(path, pattern = N.Z, recursive = TRUE))
+                      if(length(files) == 0)
+                      {
+                          N <- gsub("[^0-9]", "", N)
+                          N.A <- paste0(N, '.A')
+                          N.Z <- paste0(N, '.Z')
+                          files <- c(dir(path, pattern = N.A, recursive = TRUE),
+                                     dir(path, pattern = N.Z, recursive = TRUE))
+                      }
+                      files
+              })
+    
+    table(laply(file.list, length))
+    
+    file.list[laply(file.list, length) == 519]
     
 }
