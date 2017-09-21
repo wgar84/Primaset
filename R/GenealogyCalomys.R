f0e7704b7918db08ba31d3d689ae2511bdf1bd85
Guilherme Garcia <wgar@usp.br>
2017-09-21 10:07:49 -0300

    Genealogy and MissLM Calomys

---
+++ R/GenealogyCalomys.R
@@ -0,0 +1,37 @@
+##' @title
+##' GenealogyCalomys
+##'
+##' @description
+##' Creates inverse genealogy matrix for Calomys data.
+##'
+##' @param filled output from MissLMCalomys
+##'
+##' @return List with three elements:
+##' coordinate data (coord), specimen information (info) and genealogy information (gen),
+##' as produced by inverseA from MCMCglmm; thus, gen is itselg a list with three elements.
+##'
+##' @seealso inverseA
+##' 
+##' @author Guilherme Garcia
+##' 
+##' @importFrom MCMCglmm inverseA
+##' @importFrom plyr aaply
+
+GenealogyCalomys <- function(filled)
+    {
+        info <- filled $ info
+        ped <- info[, c('ID', 'DAM', 'SIR')]
+        p.gen <- c(unique(ped $ DAM), unique(ped $ SIR))
+        p.gen <- cbind(p.gen, rep(NA, length(p.gen)), rep(NA, length(p.gen)))
+
+        ped <- as.matrix(ped)
+        ped <- aaply(ped, 1, gsub, pattern = ' ', replace = '')
+        
+        full.ped <- rbind(p.gen, ped)
+
+        gen <- inverseA(full.ped)
+
+        list('info' = info, 'coord' = filled $ coord, 'gen' = gen)
+    }
+    
+    
---
+++ R/MissLMCalomys.R
@@ -0,0 +1,65 @@
+##' @title
+##' MissLMCalomys
+##'
+##' @description
+##' Fill up missing landmarks in Calomys database
+##'
+##' @param cleanup output from CleanUpCalomys
+##'
+##' @return
+##' list with filled configuration and ids
+##'
+##' @author Guilherme Garcia
+##'
+##' @importFrom geomorph estimate.missing
+##' @importFrom plyr alply
+##'
+
+MissLMCalomys <- function(cleanup)
+    {
+        ## do it on replicates separately
+        ## do it by age class
+
+        ## create age categories
+        info <- cleanup $ info
+        coord <- cleanup $ coord
+
+        ## duplicated names
+        info $ SIB <-
+            ifelse(duplicated(info $ ID),
+                   paste0(as.character(info $ SIB), 'S'), as.character(info $ SIB))
+        
+        info $ ID <-
+            as.factor(ifelse(duplicated(info $ ID),
+                             paste0(as.character(info $ ID), 'S'),
+                             as.character(info $ ID)))
+
+        dimnames(coord) [[4]] <- info $ ID
+        
+        out <- array(0, dim(coord))
+        dimnames(out) <- dimnames(coord)
+        
+        ## predefined based on colony (mis)design
+        info $ AGECAT <-
+            cut(info $ AGE, c(-Inf, 40, 75, 120, 239, 358, Inf))
+
+        agecat.matrix <- model.matrix(~ 0 + AGECAT, info)
+
+        alply(agecat.matrix, 2, function(agevec)
+        {
+            agevec <- as.logical(agevec)
+            R1 <- coord[, , 'R1', agevec]
+            R2 <- coord[, , 'R2', agevec]
+            
+            R1.est <- estimate.missing(R1, method = 'TPS')
+            R2.est <- estimate.missing(R2, method = 'TPS')
+            
+            out [, , 'R1', agevec] <<- R1.est
+            out [, , 'R2', agevec] <<- R2.est
+        })
+
+        cat('\n') ## or else prompt stays after last progress bar
+        
+        list('info' = info, 'coord' = out)
+
+    }
